b9759cabce882adbf482392ca2e442c3aa52d63a
Duong Thanh Duy <duydt@hasaki.vn>
2022-04-20 11:14:17 +0700

    fix cache at server side problems

--- components/search/Form.js
+++ components/search/Form.js
@@ -6,19 +6,23 @@ const SearchForm = (props) => {
 
   const searchEndpoint = (query) => `/api/search?q=${query}`;
 
-  const handleInputChange = useCallback((e) => {
-    const query = e.target.value;
-    setQuery(query);
-    if (query.length) {
-      fetch(searchEndpoint(query))
-        .then((res) => res.json())
-        .then((res) => {
-          setResults(res.results);
-        });
-    } else {
-      setResults([]);
-    }
-  });
+  const handleInputChange = useCallback(
+    (e) => {
+      const query = e.target.value;
+      setQuery(query);
+      if (query.length) {
+        fetch(searchEndpoint(query))
+          .then((res) => res.json())
+          .then((res) => {
+            setResults(res.results);
+          });
+      } else {
+        setResults([]);
+      }
+    },
+    [query]
+  );
+
   return (
     <>
       <input onBlur={handleInputChange}></input>
--- next.config.js
+++ next.config.js
@@ -12,19 +12,19 @@ const nextConfig = {
       layers: true,
     };
 
-    if (isServer) {
-      return {
-        ...config,
-        // This is what allows us to add a node script via NextJS's server
-        entry() {
-          return config.entry().then((entry) => {
-            return Object.assign({}, entry, {
-              cache: "./cache/cache.js",
-            });
-          });
-        },
-      };
-    }
+    // if (isServer) {
+    //   return {
+    //     ...config,
+    //     // This is what allows us to add a node script via NextJS's server
+    //     entry() {
+    //       return config.entry().then((entry) => {
+    //         return Object.assign({}, entry, {
+    //           cache: "./cache/cache.js",
+    //         });
+    //       });
+    //     },
+    //   };
+    // }
     return config;
   },
 };
