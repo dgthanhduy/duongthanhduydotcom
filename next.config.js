943603d5d0b890791c63073f4dd6957b3229ccf5
Duong Thanh Duy <duydt@hasaki.vn>
2022-04-20 15:54:38 +0700

    .

--- next.config.js
+++ next.config.js
@@ -4,6 +4,35 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+
+  webpack: (config, { isServer }) => {
+    // Needed if your cache script is asynchronous
+    config.experiments = {
+      topLevelAwait: true,
+      layers: true,
+    };
+
+    if (!isServer)
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+
+    if (isServer) {
+      return {
+        ...config,
+        // This is what allows us to add a node script via NextJS's server
+        entry() {
+          return config.entry().then((entry) => {
+            return Object.assign({}, entry, {
+              cache: "./lib/prebuild.js",
+            });
+          });
+        },
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
